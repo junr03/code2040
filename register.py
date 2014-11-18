8c42589ea2fcca37a82a1683be7ec8b9b155ede4
Jose Ulises Nino Rivera <jnino1@jhu.edu>
2014-11-17 23:40:57 -0500

    Added module register.py that requests the code2040 API for my token. The data is hardcoded. Considering making it user input for UI

---
+++ register.py
@@ -0,0 +1,12 @@
+# This file contains the code necessary to perform an API
+# request to the code2040 API to obtain a unique token, given
+# my email is jnino1@jhu.edu and my repository lives
+# in https://github.com/junr03/code2040
+import requests
+import json
+
+def token():
+	data = {'email':'jnino1@jhu.edu', 'github': 'https://github.com/junr03/code2040'}
+	r = requests.post('http://challenge.code2040.org/api/register', data=json.dumps(data))
+	jsonData = r.json()
+	return jsonData['result']
